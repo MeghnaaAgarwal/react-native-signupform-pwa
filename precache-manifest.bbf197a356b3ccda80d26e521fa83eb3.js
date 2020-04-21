c4a662c38473e226474911e384e2eebd02e25235
Meghna Agarwal <meghna@softtrends.local>
2020-04-21 11:04:04 +0530

    Updates

---
+++ precache-manifest.bbf197a356b3ccda80d26e521fa83eb3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "6da4b1f64059a3be66e2",
+    "url": "/static/js/app.9f5cf811.chunk.js"
+  },
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "99099ac327231d871024",
+    "url": "/static/js/2.068227cf.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "82860498638422ef4f3679795c2345fb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d502a5ce9f545cde67785d051d14be6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  }
+];
